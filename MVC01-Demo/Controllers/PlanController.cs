b8236267dc40df5a9758816144df2d2d26c4f9a5
Mariam Abdelrahman Youssef <156664021+Mariam0-0@users.noreply.github.com>
2026-06-15 18:44:37 +0300

    Fix nested repo issue (remove inner .git)

---
+++ MVC01-Demo/Controllers/PlanController.cs
@@ -0,0 +1,38 @@
+﻿using GymManagement.DAL.Repositories.Classes;
+using GymManagement.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC01_Demo.Contexts;
+
+namespace MVC01_Demo.Controllers
+{
+    public class PlanController : Controller
+    {
+        // 1) DB connection 
+        //private readonly GymDbContext context;
+
+        private readonly IPlanRepository _planRepository;
+        public PlanController(IPlanRepository planRepository)
+        {
+            _planRepository = planRepository;
+        }
+
+        // 2) Get :: BaseURL/Plan/Index
+        public async Task<IActionResult> Index(CancellationToken ct)
+        {
+            var plans = await _planRepository.GetAllAsync(ct: ct); // pass by name
+            return View(plans);
+        }
+
+        // 3) Get :: BaseURL/Plan/Details/{id}
+        public async Task<IActionResult> Details(int id, CancellationToken ct)
+        {
+            var plan = await _planRepository.GetByIdAsync(id, ct);
+            if (plan == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+    }
+}
---
+++ MVC01-Demo/Program.cs
@@ -0,0 +1,51 @@
+using GymManagement.DAL.Repositories.Classes;
+using GymManagement.DAL.Repositories.Interfaces;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+using MVC01_Demo.Contexts;
+
+namespace MVC01_Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // Register DI
+            builder.Services.AddScoped<IPlanRepository,PlanRepository>();
+            // ef core will create object from dbcontext automatic when we request it from the container (DI)
+
+            builder.Services.AddDbContext<GymDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
